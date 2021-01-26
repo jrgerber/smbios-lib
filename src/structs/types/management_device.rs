9cd4b66977a608fa160e415a18b4a8eb6dbd1eb7
Jeff Gerber <jeffger@microsoft.com>
2021-01-25 16:59:14 -0800

    management_device completed

--- src/structs/types/management_device.rs
+++ src/structs/types/management_device.rs
@@ -30,8 +30,10 @@ impl<'a> SMBiosManagementDevice<'a> {
     }
 
     /// Device's type
-    pub fn device_type(&self) -> Option<u8> {
-        self.parts.get_field_byte(0x05)
+    pub fn device_type(&self) -> Option<ManagementDeviceTypeData> {
+        self.parts
+            .get_field_byte(0x05)
+            .and_then(|raw| Some(ManagementDeviceTypeData::from(raw)))
     }
 
     /// Device's address
@@ -40,8 +42,10 @@ impl<'a> SMBiosManagementDevice<'a> {
     }
 
     /// Type of addressing used to access the device
-    pub fn address_type(&self) -> Option<u8> {
-        self.parts.get_field_byte(0x0A)
+    pub fn address_type(&self) -> Option<ManagementDeviceAddressTypeData> {
+        self.parts
+            .get_field_byte(0x0A)
+            .and_then(|raw| Some(ManagementDeviceAddressTypeData::from(raw)))
     }
 }
 
@@ -54,6 +58,156 @@ impl fmt::Debug for SMBiosManagementDevice<'_> {
             .field("address", &self.address())
             .field("address_type", &self.address_type())
             .finish()
+    }
+}
+
+/// # Management Device - Type Data
+pub struct ManagementDeviceTypeData {
+    /// Raw value
+    ///
+    /// _raw_ is most useful when _value_ is None.
+    /// This is most likely to occur when the standard was updated but
+    /// this library code has not been updated to match the current
+    /// standard.
+    pub raw: u8,
+    /// The contained [ManagementDeviceType] value
+    pub value: ManagementDeviceType,
+}
+
+impl fmt::Debug for ManagementDeviceTypeData {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct(std::any::type_name::<ManagementDeviceTypeData>())
+            .field("raw", &self.raw)
+            .field("value", &self.value)
+            .finish()
+    }
+}
+
+impl Deref for ManagementDeviceTypeData {
+    type Target = ManagementDeviceType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+/// # Management Device - Type
+#[derive(Debug, PartialEq, Eq)]
+pub enum ManagementDeviceType {
+    /// Other
+    Other,
+    /// Unknown
+    Unknown,
+    /// National Semiconductor LM75
+    NationalSemiconductorLM75,
+    /// National Semiconductor LM78
+    NationalSemiconductorLM78,
+    /// National Semiconductor LM79
+    NationalSemiconductorLM79,
+    /// National Semiconductor LM80
+    NationalSemiconductorLM80,
+    /// National Semiconductor LM81
+    NationalSemiconductorLM81,
+    /// Analog Devices ADM9240
+    AnalogDevicesADM9240,
+    /// Dallas Semiconductor DS1780
+    DallasSemiconductorDS1780,
+    /// Maxim 1617
+    Maxim1617,
+    /// Genesys GL518SM
+    GenesysGL518SM,
+    /// Winbond W83781D
+    WinbondW83781D,
+    /// Holtek HT82H791
+    HoltekHT82H791,
+    /// A value unknown to this standard, check the raw value
+    None,
+}
+
+impl From<u8> for ManagementDeviceTypeData {
+    fn from(raw: u8) -> Self {
+        ManagementDeviceTypeData {
+            value: match raw {
+                0x01 => ManagementDeviceType::Other,
+                0x02 => ManagementDeviceType::Unknown,
+                0x03 => ManagementDeviceType::NationalSemiconductorLM75,
+                0x04 => ManagementDeviceType::NationalSemiconductorLM78,
+                0x05 => ManagementDeviceType::NationalSemiconductorLM79,
+                0x06 => ManagementDeviceType::NationalSemiconductorLM80,
+                0x07 => ManagementDeviceType::NationalSemiconductorLM81,
+                0x08 => ManagementDeviceType::AnalogDevicesADM9240,
+                0x09 => ManagementDeviceType::DallasSemiconductorDS1780,
+                0x0A => ManagementDeviceType::Maxim1617,
+                0x0B => ManagementDeviceType::GenesysGL518SM,
+                0x0C => ManagementDeviceType::WinbondW83781D,
+                0x0D => ManagementDeviceType::HoltekHT82H791,
+                _ => ManagementDeviceType::None,
+            },
+            raw,
+        }
+    }
+}
+
+/// # Management Device — Address Type Data
+pub struct ManagementDeviceAddressTypeData {
+    /// Raw value
+    ///
+    /// _raw_ is most useful when _value_ is None.
+    /// This is most likely to occur when the standard was updated but
+    /// this library code has not been updated to match the current
+    /// standard.
+    pub raw: u8,
+    /// The contained [ManagementDeviceAddressType] value
+    pub value: ManagementDeviceAddressType,
+}
+
+impl fmt::Debug for ManagementDeviceAddressTypeData {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct(std::any::type_name::<ManagementDeviceAddressTypeData>())
+            .field("raw", &self.raw)
+            .field("value", &self.value)
+            .finish()
+    }
+}
+
+impl Deref for ManagementDeviceAddressTypeData {
+    type Target = ManagementDeviceAddressType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+/// # Management Device — Address Type
+#[derive(Debug, PartialEq, Eq)]
+pub enum ManagementDeviceAddressType {
+    /// Other
+    Other,
+    /// Unknown
+    Unknown,
+    /// I/O Port
+    IOPort,
+    /// Memory
+    Memory,
+    /// SM Bus
+    SMBus,
+    /// A value unknown to this standard, check the raw value
+    None,
+}
+
+impl From<u8> for ManagementDeviceAddressTypeData {
+    fn from(raw: u8) -> Self {
+        ManagementDeviceAddressTypeData {
+            value: match raw {
+                0x01 => ManagementDeviceAddressType::Other,
+                0x02 => ManagementDeviceAddressType::Unknown,
+                0x03 => ManagementDeviceAddressType::IOPort,
+                0x04 => ManagementDeviceAddressType::Memory,
+                0x05 => ManagementDeviceAddressType::SMBus,
+                _ => ManagementDeviceAddressType::None,
+            },
+            raw,
+        }
     }
 }
 
@@ -72,8 +226,14 @@ mod tests {
         let test_struct = SMBiosManagementDevice::new(&parts);
 
         assert_eq!(test_struct.description(), Some("LM78-1".to_string()));
-        assert_eq!(test_struct.device_type(), Some(4));
+        assert_eq!(
+            *test_struct.device_type().unwrap(),
+            ManagementDeviceType::NationalSemiconductorLM78
+        );
         assert_eq!(test_struct.address(), Some(0));
-        assert_eq!(test_struct.address_type(), Some(3));
+        assert_eq!(
+            *test_struct.address_type().unwrap(),
+            ManagementDeviceAddressType::IOPort
+        );
     }
 }
