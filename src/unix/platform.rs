28db4de59b632787eb0ba6fba99747837f1cc0a4
Jeff Gerber <jeffger@microsoft.com>
2021-02-19 19:56:16 -0800

    Revert "elevate linux testing"
    This reverts commit 09c5dc726a33e0e77885c7bfc4c409983b985c5a.
    sudo cannot find cargo

--- src/unix/platform.rs
+++ src/unix/platform.rs
@@ -71,8 +71,9 @@ pub fn table_load_from_device() -> Result<SMBiosData, Error> {
 mod tests {
     use super::*;
 
-    // This test must be run elevated
+    // This test must be run elevated and we ignore it for test passes
     #[test]
+    #[ignore]
     fn device_load_unit_test() {
         match table_load_from_device() {
             Ok(table) => {
