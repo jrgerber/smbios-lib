0d0a20cf67c92a53885ba23de435f53ca965126f
Jeff Gerber <jeffger@microsoft.com>
2021-02-24 05:38:24 -0800

    Revert "remove iokit crate and use build.rs"
    This reverts commit 1ab3ccc88b68c325b6fe34b81173303a7f61e70a.
    Juan has a working solution and this commit steps on the base
    he forked from.

--- src/macos/platform.rs
+++ src/macos/platform.rs
@@ -1,77 +1,29 @@
 use crate::*;
-
-use core_foundation::{
-    base::CFRelease,
-    data::{CFDataGetBytePtr, CFDataGetLength, CFDataRef},
-    string::CFStringRef,
-};
 use core_foundation::{
     base::{kCFAllocatorDefault, mach_port_t, TCFTypeRef},
-    dictionary::CFDictionaryGetValueIfPresent,
+    data::CFDataGetLength,
+    dictionary::{CFDictionaryGetValueIfPresent, CFMutableDictionaryRef},
 };
-use core_foundation_sys::base::{CFAllocatorRef, CFTypeRef};
-use core_foundation_sys::dictionary::{CFDictionaryRef, CFMutableDictionaryRef};
-
-use mach::kern_return::kern_return_t;
-
-use std::os::raw::{c_char, c_int, c_uint, c_void};
-
-/*
+use core_foundation::{
+    base::{CFRelease},
+    data::{CFDataGetLength, CFDataGetBytePtr, CFDataRef},
+};
+//use core_foundation_sys::dictionary::CFMutableDictionaryRef;
 use io_kit_sys::{
     ret::kIOReturnSuccess,
     types::{io_service_t, IOOptionBits},
     IOMasterPort, IOObjectRelease, IORegistryEntryCreateCFProperties,
     IORegistryEntryCreateCFProperty, IOServiceGetMatchingService, IOServiceMatching, CFSTR,
 };
-*/
 use mach::*;
-use std::{convert::TryFrom, ffi::CString, io::Error, io::ErrorKind, ptr::null_mut};
-///*
-pub type io_registry_entry_t = io_object_t;
-pub type io_service_t = io_object_t;
-pub type io_object_t = mach_port_t;
-pub type IOOptionBits = c_uint;
-pub const KERN_SUCCESS: kern_return_t = 0;
-// IOReturn
-pub type IOReturn = kern_return_t;
-// OK
-pub const kIOReturnSuccess: IOReturn = KERN_SUCCESS as c_int;
+use std::{
+    convert::TryFrom,
+    ffi::{c_void, CString},
+    io::Error,
+    io::ErrorKind,
+    ptr::null_mut,
+};
 
-extern "C" {
-    pub static kIOMasterPortDefault: mach_port_t;
-
-    fn __CFStringMakeConstantString(cStr: *const c_char) -> CFStringRef;
-
-    pub fn IOMasterPort(bootstrapPort: mach_port_t, masterPort: *mut mach_port_t) -> kern_return_t;
-
-    pub fn IORegistryEntryCreateCFProperties(
-        entry: io_registry_entry_t,
-        properties: *mut CFMutableDictionaryRef,
-        allocator: CFAllocatorRef,
-        options: IOOptionBits,
-    ) -> kern_return_t;
-
-    pub fn IORegistryEntryCreateCFProperty(
-        entry: io_registry_entry_t,
-        key: CFStringRef,
-        allocator: CFAllocatorRef,
-        options: IOOptionBits,
-    ) -> CFTypeRef;
-
-    pub fn IOServiceGetMatchingService(
-        masterPort: mach_port_t,
-        matching: CFDictionaryRef,
-    ) -> io_service_t;
-
-    pub fn IOServiceMatching(name: *const c_char) -> CFMutableDictionaryRef;
-
-    pub fn IOObjectRelease(object: io_object_t) -> kern_return_t;
-}
-
-pub fn CFSTR(cStr: *const c_char) -> CFStringRef {
-    unsafe { __CFStringMakeConstantString(cStr) }
-}
-//*/
 struct AppleSMBiosService {
     pub service_handle: io_service_t,
 }
@@ -151,7 +103,7 @@ fn try_load_macos_table() -> Result<Vec<u8>, Error> {
     unsafe {
         let smbios_table_name = CString::new("SMBIOS").expect("CString::new failed");
         let option_bits: IOOptionBits = 0;
-        let mut properties: CFMutableDictionaryRef = null_mut();
+        let properties: CFMutableDictionaryRef = null_mut();
         let properties_ptr: *mut CFMutableDictionaryRef = &mut properties;
 
         println!("here");
@@ -170,8 +122,6 @@ fn try_load_macos_table() -> Result<Vec<u8>, Error> {
             ));
         }
 
-        println!("here2");
-
         let mut data_ref: CFDataRef = null_mut();
         let data_ref_ptr: *mut CFDataRef = &mut data_ref;
 
@@ -187,14 +137,12 @@ fn try_load_macos_table() -> Result<Vec<u8>, Error> {
             ));
         }
 
-        println!("here3");
+        let len = CFDataGetLength(data_ref);
 
-        let data_ptr = CFDataGetBytePtr(data_ref);
-        let data_length = CFDataGetLength(data_ref);
-        let mut table = Vec::with_capacity(data_length as usize);
+        let mut table: Vec<u8> = Vec::with_capacity(len as usize);
+        let table_ptr = table.as_mut_ptr();
 
-        std::ptr::copy(data_ptr, table.as_mut_ptr(), data_length as usize);
-        table.set_len(data_length as usize);
+        CFDataGetBytes(data_ref, CFRange::init(0, len), table_ptr);
 
         if !data_ref.is_null() {
             CFRelease(data_ref.as_void_ptr());
