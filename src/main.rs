49f0d1ca0f228d977ed7bf64c88ca79b1240ec13
Juan Zuluaga <juzuluag@hotmail.com>
2021-03-02 19:08:16 -0500

    Set the correct option

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use smbioslib::*;
 
+
+fn print_usage(program: &str, opts: getopts::Options) {
+	let brief = format!("Usage: {} -f FILE", program);
+	print!("{}", opts.usage(&brief));
+}
+
 fn main() {
     let file_option = "f";
-
     let args: Vec<String> = std::env::args().collect();
     let mut opts = getopts::Options::new();
     opts.optopt(file_option, "", "smbios table filename", "FILE");
 
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { println!("Option: {:?}" , m); m },
+        Ok(m) => { m },
         Err(err) => {
-            println!("No option specified. Getting table from device");
-            match table_load_from_device() {
-                Ok(table) => {
-                    println!("table_data: {:#?}", table);
-                    return
-                },
-                Err(err) => { panic!("Unable to load table from device: {:#?}", err) }
-            }
+            print_usage(&args[0].clone(), opts);
+            std::process::exit(1);
         }
     };
 
@@ -31,6 +30,15 @@ fn main() {
                 }
             }
         },
-        None => { panic!("filename expected") }
+        None => {
+            println!("No option specified. Getting table from device");
+            match table_load_from_device() {
+                Ok(table) => {
+                    println!("table_data: {:#?}", table);
+                    return
+                },
+                Err(err) => { panic!("Unable to load table from device: {:#?}", err) }
+            }
+         }
     }
 }
