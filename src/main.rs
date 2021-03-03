62f54da09831d5f39b0272e548f1fb1fde8883b5
Juan Zuluaga <juzuluag@hotmail.com>
2021-03-03 18:38:02 -0500

    Simplify main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use smbioslib::*;
 
 
-fn print_usage(program: &str, opts: getopts::Options) {
-	let brief = format!("Usage: {} [-f|-o FILE]", program);
-	print!("{}", opts.usage(&brief));
-}
-
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_option = "f";
     let output_option = "o";
 
@@ -15,58 +10,26 @@ fn main() {
     opts.optopt(file_option, "", "read smbios table from file", "FILE");
     opts.optopt(output_option, "", "dump smbios table to a file", "FILE");
 
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m },
-        Err(err) => {
-            eprintln!("Error: {:?}", err);
-            print_usage(&args[0].clone(), opts);
-            std::process::exit(1);
-        }
-    };
+    let matches = opts.parse(&args[1..])?;
 
-    if !matches.opt_present(file_option) && 
-        !matches.opt_present(output_option) {
-        match table_load_from_device() {
-            Ok(table) => {
-                println!("table_data: {:#?}", table);
-                return;
-            },
-            Err(err) => {
-                panic!("Unable to load table from device: {:#?}", err)
-            }
-        }
-	}
+    if !matches.opt_present(file_option) &&
+       !matches.opt_present(output_option) {
+        println!("table_data: {:#?}", table_load_from_device()?);
+        return Ok(())
+    }
 
     match matches.opt_str(file_option) {
         Some(filename) => {
-            match load_smbios_data_from_file(&filename) {
-                Ok(table) => {
-                    println!("Load table from file: {} \n{:#?}", &filename, table);
-                    return
-                },
-                Err(err) => {
-                    eprintln!("Error loading from file: {:?}", err);
-                    std::process::exit(1);
-                }
-            }
+            println!("Load table from file: {} \n{:#?}", &filename,
+                load_smbios_data_from_file(&filename)?);
         },
         None => ()
     }
-
     match matches.opt_str(output_option) {
         Some(filename) => {
-            match raw_smbios_from_device() {
-                Ok(raw) => {
-                    match dump_raw(raw, &filename) {
-                        Err(err) => panic!("{}", err),
-                        Ok(_) => return
-                    }
-                },
-                Err(err) => {
-                    panic!("Unable to load table from device: {:#?}", err)
-                }
-            }
-        }
+            dump_raw(raw_smbios_from_device()?, &filename)?;
+        },
         None => ()
     }
+    Ok(())
 }
